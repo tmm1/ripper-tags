a551d375866e2d1a1a90a0b7bfa3f59c5a662c73
Anton Bangratz <anton.bangratz@gmail.com>
2013-07-01 16:42:49 +0200

    Add emacs formatter

---
+++ lib/tag_ripper/emacs_formatter.rb
@@ -0,0 +1,15 @@
+class TagRipper
+  class EmacsFormatter
+    def initialize(tags)
+      @tags = tags
+    end
+
+    def build
+      data = []
+      @tags.each do |tag|
+        data << "#{tag[:pattern]}\x7F#{tag[:name]}\x01#{tag[:line]},0"
+      end
+      data.join("\n")
+    end
+  end
+end
