8c1ac83a2ca0de27ed98cb6800c5e9a3690409ba
Mislav Marohnić <mislav.marohnic@gmail.com>
2013-08-14 14:03:07 +0200

    move CLI logic under RipperTags and invoke it explicitly
    Before, the bare act of `require "ripper-tags"` would process ARGV and
    start generating tags. This is unwanted and potentially destructive
    behavior if the library is loaded in an application, e.g. added to
    Gemfile of a Rails app.
    
    This moves all CLI logic to methods under RipperTags and requires the
    `ripper-tags` executable to trigger the behavior explicitly. Bonus
    side-effect is that the whole thing is now testable.

--- lib/ripper-tags.rb
+++ lib/ripper-tags.rb
@@ -7,90 +7,105 @@ require 'ripper-tags/emacs_formatter'
 require 'ripper-tags/vim_formatter'
 require 'ripper-tags/json_formatter'
 
-options = OpenStruct.new(
-  json: false,
-  debug: false,
-  vim: false,
-  emacs: false,
-  tag_file_name: "./tags",
-  verbose_debug: false,
-  verbose: false,
-  force: false,
-  files: %w[.],
-  recursive: false,
-  all_files: false
-)
+module RipperTags
+  def self.default_options
+    OpenStruct.new \
+      json: false,
+      debug: false,
+      vim: false,
+      emacs: false,
+      tag_file_name: "./tags",
+      verbose_debug: false,
+      verbose: false,
+      force: false,
+      files: %w[.],
+      recursive: false,
+      all_files: false
+  end
 
-all_tags = []
+  def self.option_parser(options)
+    OptionParser.new do |opts|
+      opts.banner = "Usage: #{opts.program_name} [options] FILES..."
 
-opt_parse = OptionParser.new do |opts|
-  opts.banner = "Usage: #{opts.program_name} [options] FILES..."
-  opts.separator ""
-  opts.on("-e", "--emacs", "Output emacs format to tags file") do
-    options.emacs = true
-  end
-  opts.on("-f", "--tag-file (FILE|-)", "Filename to output tags to, default #{options.tag_file_name}",
-         '"-" outputs to standard output') do |fname|
-    options.tag_file_name = fname
-  end
-  opts.on("-J", "--json", "Output nodes as json") do
-    options.json = true
-  end
-  opts.on("-A", "--all-files", "Parse all files as ruby files") do
-    options.all_files = true
-  end
-  opts.on("-R", "--recursive", "Descend recursively into given directory") do
-    options.recursive = true
-  end
-  opts.on("-V", "--vim", "Output vim optimized format to tags file") do
-    options.vim = true
-  end
-  opts.separator " "
-  opts.on_tail("-d", "--debug", "Output parse tree") do
-    options.debug = true
-  end
-  opts.on_tail("--debug-verbose", "Output parse tree verbosely") do
-    options.verbose_debug = true
-  end
-  opts.on_tail("-v", "--verbose", "Print additional information on stderr") do
-    options.verbose = true
-  end
-  opts.on_tail("--force", "Skip files with parsing errors") do
-    options.force = true
-  end
-  opts.on_tail("-h", "--help", "Show this message") do
-    $stderr.puts opts
-    exit
-  end
-end
-opt_parse.parse!(ARGV)
+      opts.separator ""
 
-if ARGV.size > 0
-  options.files = ARGV
-else
-  $stderr.puts opt_parse
-  exit
-end
+      opts.on("-e", "--emacs", "Output emacs format to tags file") do
+        options.emacs = true
+      end
+      opts.on("-f", "--tag-file (FILE|-)", "Filename to output tags to, default #{options.tag_file_name}",
+             '"-" outputs to standard output') do |fname|
+        options.tag_file_name = fname
+      end
+      opts.on("-J", "--json", "Output nodes as json") do
+        options.json = true
+      end
+      opts.on("-A", "--all-files", "Parse all files as ruby files") do
+        options.all_files = true
+      end
+      opts.on("-R", "--recursive", "Descend recursively into given directory") do
+        options.recursive = true
+      end
+      opts.on("-V", "--vim", "Output vim optimized format to tags file") do
+        options.vim = true
+      end
 
-tags = RipperTags::DataReader.new(options).read.flatten
+      opts.separator " "
 
+      opts.on_tail("-d", "--debug", "Output parse tree") do
+        options.debug = true
+      end
+      opts.on_tail("--debug-verbose", "Output parse tree verbosely") do
+        options.verbose_debug = true
+      end
+      opts.on_tail("-v", "--verbose", "Print additional information on stderr") do
+        options.verbose = true
+      end
+      opts.on_tail("--force", "Skip files with parsing errors") do
+        options.force = true
+      end
+      opts.on_tail("-h", "--help", "Show this message") do
+        $stderr.puts opts
+        exit
+      end
 
-formatter = if options.vim
-              RipperTags::VimFormatter
-            elsif options.emacs
-              RipperTags::EmacsFormatter
-            elsif options.json
-              RipperTags::JSONFormatter
-            else
-              RipperTags::DefaultFormatter
-            end
+      yield(opts, options) if block_given?
+    end
+  end
 
-if tags && !tags.empty?
-  if options.tag_file_name == '-'
-    $stdout.print(formatter.new(tags).build)
-  else
-    File.open(options.tag_file_name, "w+") do |tag_file|
-      tag_file.print(formatter.new(tags).build)
+  def self.process_args(argv, run = method(:run))
+    option_parser(default_options) do |optparse, options|
+      file_list = optparse.parse(argv)
+      if !file_list.empty? then options.files = file_list
+      elsif !options.recursive then abort(optparse.banner)
+      end
+      return run.call(options)
+    end
+  end
+
+  def self.formatter_for(options)
+    if options.vim
+      RipperTags::VimFormatter
+    elsif options.emacs
+      RipperTags::EmacsFormatter
+    elsif options.json
+      RipperTags::JSONFormatter
+    else
+      RipperTags::DefaultFormatter
+    end
+  end
+
+  def self.run(options)
+    tags = RipperTags::DataReader.new(options).read.flatten
+    formatter = formatter_for(options)
+
+    if tags && !tags.empty?
+      if options.tag_file_name == '-'
+        $stdout.print(formatter.new(tags).build)
+      else
+        File.open(options.tag_file_name, "w+") do |tag_file|
+          tag_file.print(formatter.new(tags).build)
+        end
+      end
     end
   end
 end
---
+++ test/test_cli.rb
@@ -0,0 +1,46 @@
+require 'test/unit'
+require 'stringio'
+require 'ripper-tags'
+
+class CliTest < Test::Unit::TestCase
+  def process_args(argv)
+    RipperTags.process_args(argv, lambda {|o| o})
+  end
+
+  def test_empty_args
+    err = assert_raise(SystemExit) do
+      with_program_name('ripper-tags') do
+        capture_stderr do
+          RipperTags.process_args([])
+        end
+      end
+    end
+    assert_equal "Usage: ripper-tags [options] FILES...", err.message
+  end
+
+  def test_recurse_defaults_to_current_dir
+    options = process_args(%w[-R])
+    assert_equal true, options.recursive
+    assert_equal %w[.], options.files
+  end
+
+  def with_program_name(name)
+    old_name = $0
+    $0 = name
+    begin
+      yield
+    ensure
+      $0 = old_name
+    end
+  end
+
+  def capture_stderr
+    old_stderr = $stderr
+    $stderr = StringIO.new
+    begin
+      yield
+    ensure
+      $stderr = old_stderr
+    end
+  end
+end
