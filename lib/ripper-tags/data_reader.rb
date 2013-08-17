6d77fb7bb08106fc7d2bc609cddb89deb59483b6
Mislav Marohnić <mislav.marohnic@gmail.com>
2013-08-17 17:19:00 +0200

    refactor DataReader#each_tag
    Now logs debugging info to stderr

--- lib/ripper-tags/data_reader.rb
+++ lib/ripper-tags/data_reader.rb
@@ -90,14 +90,7 @@ module RipperTags
       file_finder.each_file do |file|
         begin
           $stderr.puts "Parsing file #{file}" if options.verbose
-          file_contents = read_file(file)
-          sexp = Parser.new(file_contents, file).parse
-          visitor = Visitor.new(sexp, file, file_contents)
-          if options.verbose_debug
-            pp Ripper.sexp(file_contents)
-          elsif options.debug
-            pp sexp
-          end
+          extractor = tag_extractor(file)
         rescue => err
           if options.force
             $stderr.puts "Error parsing `#{file}': #{err.message}"
@@ -105,11 +98,28 @@ module RipperTags
             raise err
           end
         else
-          visitor.tags.each do |tag|
+          extractor.tags.each do |tag|
             yield tag
           end
         end
       end
+    end
+
+    def debug_dump(obj)
+      pp(obj, $stderr)
+    end
+
+    def parse_file(contents, filename)
+      sexp = Parser.new(contents, filename).parse
+      debug_dump(sexp) if options.debug
+      sexp
+    end
+
+    def tag_extractor(file)
+      file_contents = read_file(file)
+      debug_dump(Ripper.sexp(file_contents)) if options.verbose_debug
+      sexp = parse_file(file_contents, file)
+      Visitor.new(sexp, file, file_contents)
     end
   end
 end
