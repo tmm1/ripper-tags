a8473d73658b75368ba326a000921b1ab16e7bf3
Mislav Marohnić <mislav.marohnic@gmail.com>
2015-07-28 18:15:49 -0700

    ripper-tags 0.2.0

--- lib/ripper-tags.rb
+++ lib/ripper-tags.rb
@@ -9,7 +9,7 @@ require 'ripper-tags/vim_formatter'
 require 'ripper-tags/json_formatter'
 
 module RipperTags
-  def self.version() "0.1.3" end
+  def self.version() "0.2.0" end
 
   FatalError = Class.new(RuntimeError)
 
